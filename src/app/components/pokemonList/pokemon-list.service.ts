d6bd1034ecc6b52d938114983a07608683a39552
Liam <liamcavens@googlemail.com>
2019-06-02 23:13:10 +0100

    HTML fix

--- src/app/components/pokemonList/pokemon-list.service.ts
+++ src/app/components/pokemonList/pokemon-list.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from "@angular/core";
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpResponse
-} from "@angular/common/http";
+import { HttpClient,HttpErrorResponse} from "@angular/common/http";
 import { Observable, throwError, BehaviorSubject, forkJoin } from "rxjs";
 import { catchError, retry, map, flatMap } from "rxjs/operators";
 import { Pokemon } from "./pokemon";
